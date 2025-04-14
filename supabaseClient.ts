27eaeead0d8ac2365fba4985cfab844cc0445f52
andreasjpappas021 <andreas.j.pappas@gmail.com>
2025-04-14 07:52:01 -0400

    Update and rename supabaseClient.ts to lib/supabaseClient.ts

--- supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@/lib/supabaseClient'
-
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
